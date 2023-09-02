b29916d0c7a564ced3d8a3e7f64e62a394015fe6
H.N. Vignolles <hn.vignolles@gmail.com>
2023-09-02 01:33:03 -0300

    update

--- model.py
+++ model.py
@@ -6,23 +6,33 @@ from matplotlib.ticker import (AutoMinorLocator, MultipleLocator)
 df = pd.read_csv("model.csv", sep="\t")
 display(df.head(6))
 
-t, v1 = df['time'], df['V(Reset,N003)']
+fig = plt.figure(figsize=(32,4))
+ax1 = fig.add_subplot(1,1,1)
 
-fig = plt.figure(figsize=(12,4))
-ax1 = fig.add_subplot(1,2,1)
-
-ax1.plot(t, v1, label="...")
-
-ax1.set_title("...")
+t = df['time']*1000
+VB1, VRE, VRG, VP = df['V(vl)'], df['V(reset)'], df['V(Reset,R-PI_GND)'], df['V(pulse)']
+ax1.plot(t, VRE, 'g-', label="V(reset)")
 ax1.legend()
-#ax.set_xlim(1, 1.3)
-#ax1.set_ylim(4, 5)
-#ax1.set_xlabel("Time")
-ax1.set_ylabel("V(Reset,N003)")
-#ax1.xaxis.set_major_locator(MultipleLocator(0.5))
-#ax1.xaxis.set_minor_locator(AutoMinorLocator(10))
-#ax1.yaxis.set_major_locator(MultipleLocator(0.25))
-#ax1.yaxis.set_minor_locator(AutoMinorLocator(10))
-#ax1.grid(which='major', color='#CCCCCC', linestyle='--')
-#ax1.grid(which='minor', color='#CCCCCC', linestyle=':')
+ax1.set_ylim(-350, 350)
+ax1.set_xlabel("t (ms)")
+ax1.set_ylabel("V(reset)")
+
+ax2 = ax1.twinx()
+ax2.plot(t, VRG, 'r-', label="V(Reset,R-PI_GND)")
+ax2.legend()
+ax2.set_ylim(-1, 5)
+ax2.set_ylabel("V(Reset,R-PI_GND)")
+ax2.xaxis.set_major_locator(MultipleLocator(100))
+ax2.xaxis.set_minor_locator(AutoMinorLocator(10))
+ax2.yaxis.set_major_locator(MultipleLocator(1))
+ax2.yaxis.set_minor_locator(AutoMinorLocator(10))
+ax2.grid(which='major', color='#CCCCCC', linestyle='--')
+ax2.grid(which='minor', color='#CCCCCC', linestyle=':')
+
+ax3 = ax1.twinx()
+ax3.plot(t, VP, 'b-', label="V(pulse)")
+ax3.legend()
+ax3.set_ylim(-2, 4)
+ax3.tick_params(right=False, labelright=False)
+
 plt.show()
